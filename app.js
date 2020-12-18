26ba1eec57520fc708839da774a1593f29853528
fyang18 <francis.yang@hotmail.fr>
2020-12-18 15:52:54 +0100

    add html css white mode

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello world !");
+/*var dark = document.getElementById("dark");
+dark.addEventListener("click", darkMode());*/
+
+function darkMode () {
+    console.log("darkMode activate !");
+
+}
